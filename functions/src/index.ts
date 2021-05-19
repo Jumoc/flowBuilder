9cd7ecd3ac6a3dddd53d83c126e268aed069f592
Jumoc <juanpcaicedo2@gmail.com>
2021-05-18 23:36:26 -0500

    Add (agent create, intent create, cors implementation): added enpoints to create and update agents and create intents

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,50 @@
 import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+import * as dialogflow from "@google-cloud/dialogflow";
+import * as express from "express";
+import * as cors from "cors";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.route("/agents/:id")
+    .get((req, res) => {
+      res.send("You send a get request to agents");
+    })
+    .post(async (req, res) => {
+      const client = new dialogflow.v2beta1.IntentsClient(
+          {keyFilename: "./flowBuilder.json"}
+      );
+      try {
+        // Create the path of the parent project
+        const formattedParent = client.projectAgentPath(req.params.id);
+
+        // Setup intent for the agent
+        const intent = req.body;
+
+        const resp = await client.createIntent({
+          parent: formattedParent,
+          intent: intent,
+        });
+        res.send(resp);
+      } catch (err) {
+        res.status(500).send(err);
+        console.error(err);
+      }
+    });
+
+app.post("/agents", async (req, res) => {
+  const client = new dialogflow.v2.AgentsClient(
+      {keyFilename: "./flowBuilder.json"}
+  );
+  try {
+    const resp = await client.setAgent({agent: req.body});
+    res.send(resp);
+  } catch (err) {
+    res.status(500).send(err);
+    console.error(err);
+  }
+});
+
+exports.app = functions.https.onRequest(app);
